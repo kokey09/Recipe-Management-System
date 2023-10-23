8c513cc3924c4b00aab8fed268d30e42d6e4b82b
KHIM <146093792+kokey09@users.noreply.github.com>
2023-10-23 22:02:58 +0800

    updatee

---
+++ database/ingredients.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM recipedb.ingredients
+LIMIT 0, 1000
+
+-- Date: 2023-10-23 21:59
+*/
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (1,'Chicken','fasd');
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (2,'Rice ','afdsa');
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (3,'Egg ','fasd');
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (4,'Pork   ','fsad');
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (5,'pasta','afsd');
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (6,'Fish','asfd');
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (7,'Banana','afsda');
+INSERT INTO `` (`ingredient_id`,`name`,`description`) VALUES (8,'carrot','fasdfasd');
