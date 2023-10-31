b940a7cef97ab213f0ddfefd75aa439dd75b0d61
KHIM <146093792+kokey09@users.noreply.github.com>
2023-10-31 19:31:34 +0800

    fixv4

--- database/reviews.sql
+++ database/reviews.sql
@@ -7,12 +7,11 @@ LIMIT 0, 1000
 
 USE recipedb;
 
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (20,1,9,'fasd',4,'2023-10-29 18:55:17');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (32,1,10,'yahallo',4,'2023-10-29 19:42:17');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (33,5,10,'yuck',1,'2023-10-29 19:43:02');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (34,1,1,'hi',5,'2023-10-29 20:08:24');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (35,1,9,'ahahahahahahahahah',3,'2023-10-29 20:19:45');
-
+INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (20, 1, 9, 'fasd', 4, '2023-10-29 18:55:17');
+INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (32, 1, 10, 'yahallo', 4, '2023-10-29 19:42:17');
+INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (33, 5, 10, 'yuck', 1, '2023-10-29 19:43:02');
+INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (34, 1, 1, 'hi', 5, '2023-10-29 20:08:24');
+INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (35, 1, 9, 'ahahahahahahahahah', 3, '2023-10-29 20:19:45');
 
 
 
