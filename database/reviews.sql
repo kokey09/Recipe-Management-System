4c0fbaff2e61256a3709d4c381446ba7197766ea
KHIM <146093792+kokey09@users.noreply.github.com>
2023-10-31 19:42:37 +0800

    fix v5

--- database/reviews.sql
+++ database/reviews.sql
@@ -7,12 +7,14 @@ LIMIT 0, 1000
 
 USE recipedb;
 
-INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (20, 1, 9, 'fasd', 4, '2023-10-29 18:55:17');
-INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (32, 1, 10, 'yahallo', 4, '2023-10-29 19:42:17');
-INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (33, 5, 10, 'yuck', 1, '2023-10-29 19:43:02');
-INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (34, 1, 1, 'hi', 5, '2023-10-29 20:08:24');
-INSERT INTO `reviews` (`review_id`, `recipe_id`, `account_id`, `review_text`, `rating`, `timestamp`) VALUES (35, 1, 9, 'ahahahahahahahahah', 3, '2023-10-29 20:19:45');
-
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (47,3,26,'mema',4,'2023-10-30 22:24:55');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (48,3,27,'luh',4,'2023-10-30 22:36:28');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (49,NULL,22,'Yummy',5,'2023-10-30 23:33:42');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (50,1,22,'yummy ',4,'2023-10-31 13:50:04');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (51,2,25,'yahalo',5,'2023-10-31 14:05:42');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (52,3,25,'Ahahaaha',5,'2023-10-31 14:09:11');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (53,14,22,'mema',4,'2023-10-31 15:02:45');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`) VALUES (54,4,22,'Mema',4,'2023-10-31 15:05:40');
 
 
 
