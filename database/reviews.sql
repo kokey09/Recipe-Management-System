439afcb0b86410f667f848c8739b50e2525e2c2a
KHIM <146093792+kokey09@users.noreply.github.com>
2023-11-03 21:59:06 +0800

    fix db v2

--- database/reviews.sql
+++ database/reviews.sql
@@ -7,14 +7,14 @@ LIMIT 0, 1000
 
 USE recipedb;
 
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (47,3,26,'mema',4,'2023-10-30 22:24:55','NULL');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (48,3,27,'luh',4,'2023-10-30 22:36:28','NULL');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (49,NULL,22,'Yummy',5,'2023-10-30 23:33:42','NULL');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (50,1,22,'yummy ',4,'2023-10-31 13:50:04','NULL');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (51,2,25,'yahalo',5,'2023-10-31 14:05:42','NULL');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (52,3,25,'Ahahaaha',5,'2023-10-31 14:09:11','NULL');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (53,14,22,'mema',4,'2023-10-31 15:02:45','NULL');
-INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (54,4,22,'Mema',4,'2023-10-31 15:05:40','NULL');
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (47,3,26,'mema',4,'2023-10-30 22:24:55',NULL);
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (48,3,27,'luh',4,'2023-10-30 22:36:28',NULL);
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (49,NULL,22,'Yummy',5,'2023-10-30 23:33:42',NULL);
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (50,1,22,'yummy ',4,'2023-10-31 13:50:04',NULL);
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (51,2,25,'yahalo',5,'2023-10-31 14:05:42',NULL);
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (52,3,25,'Ahahaaha',5,'2023-10-31 14:09:11',NULL);
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (53,14,22,'mema',4,'2023-10-31 15:02:45',NULL);
+INSERT INTO `reviews` (`review_id`,`recipe_id`,`account_id`,`review_text`,`rating`,`timestamp`,`image_url`) VALUES (54,4,22,'Mema',4,'2023-10-31 15:05:40',NULL);
 
 
 
