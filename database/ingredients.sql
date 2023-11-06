9acb0ef5f96c2ff3fded4e1dfba006c4269fffd2
KHIM <146093792+kokey09@users.noreply.github.com>
2023-11-06 22:36:58 +0800

    balik yung database folder

---
+++ database/ingredients.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM recipedb.ingredients
+LIMIT 0, 1000
+
+-- Date: 2023-10-23 21:59
+*/
+USE recipedb;
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (1,'Chicken','fasd');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (2,'Rice ','afdsa');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (3,'Egg ','fasd');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (4,'Pork   ','fsad');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (5,'pasta','afsd');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (6,'Fish','asfd');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (7,'Banana','afsda');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (10,'Rice na malagkit','asfdas');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (11,'Itiks yo','yoo');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (16,'Peach','Peach');
+INSERT INTO `ingredients` (`ingredient_id`,`name`,`description`) VALUES (17,'Apples','Fruit');
---
+++ database/recipe_ingredients.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM recipedb.recipe_ingredients
+LIMIT 0, 1000
+
+-- Date: 2023-10-23 21:58
+*/
+USE recipedb;
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (4,2);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (6,1);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (5,3);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (5,11);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (34,16);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (35,17);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (35,3);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (39,2);
+INSERT INTO `recipe_ingredients` (`recipe_id`,`ingredient_id`) VALUES (40,10);
