c894043ee75a6a36959a1437d4df861ccb0f07f9
KHIM <146093792+kokey09@users.noreply.github.com>
2023-11-23 16:59:40 +0800

    db favorites

---
+++ database/favorites.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM recipedb.favorites
+LIMIT 0, 1000
+
+-- Date: 2023-11-23 16:57
+*/
+USE recipedb;
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (1,80,41,'2023-11-23 06:46:17');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (2,80,36,'2023-11-23 06:46:42');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (3,80,37,'2023-11-23 06:47:06');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (4,79,37,'2023-11-23 06:52:10');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (5,76,37,'2023-11-23 06:52:18');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (6,87,37,'2023-11-23 06:53:56');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (7,81,37,'2023-11-23 07:02:16');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (8,75,37,'2023-11-23 07:02:31');
+INSERT INTO `favorites` (`favorite_id`,`recipe_id`,`account_id`,`timestamp`) VALUES (9,87,36,'2023-11-23 08:14:25');
