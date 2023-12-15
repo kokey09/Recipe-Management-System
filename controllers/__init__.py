06e9338653f291a69386ecaa2160bfeaa5b6c625
KHIM <146093792+kokey09@users.noreply.github.com>
2023-12-15 18:42:36 +0800

    fixing reset password with mail verification on codespace

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -19,9 +19,14 @@ def create_app():
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'khimrata11@gmail.com'  # Replace with your actual email
-    app.config['MAIL_PASSWORD'] = 'mlxo tdne vatb pmax '  # Replace with your actual password or App Password
+    app.config['MAIL_PASSWORD'] = 'mlxo tdne vatb pmax '  # Replace with your App Password
     app.secret_key = 'your_secret_key'
 
+    if 'CODESPACES' in os.environ:
+        app.config['SERVER_NAME'] = 'cautious-spoon-5000.githubpreview.dev'
+    else:
+        app.config['SERVER_NAME'] = '127.0.0.1:5000'
+        
     db.init_app(app)
     mail.init_app(app)
 
