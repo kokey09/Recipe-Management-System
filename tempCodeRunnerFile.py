2e9e8929021b00588469ad7340efae6bdb796584
KHIM <146093792+kokey09@users.noreply.github.com>
2023-12-17 00:47:15 +0800

    .

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-from controllers import create_app
-from models.account import create_default_admin
-
-app = create_app()
-create_default_admin(app)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000,debug=True)
