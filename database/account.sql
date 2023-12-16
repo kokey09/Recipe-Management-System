74e8b64157403ef3c0ac221405843fb9e35c02fd
KHIM <146093792+kokey09@users.noreply.github.com>
2023-12-16 11:53:38 +0800

    changes status verified

--- database/account.sql
+++ database/account.sql
@@ -5,8 +5,8 @@ LIMIT 0, 1000
 -- Date: 2023-12-16 11:50
 */
 USE recipedb;
-INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (36,'admin','admin@gmail.com','$2b$12$rt5h18AnAGaSsZ6VEsfUb.Mv4ECXM9OxyZYvgjKuo7ObqBwaNT5TK','2023-11-20 05:19:30','admin',0,'unverified');
-INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (37,'khim','khimrata@yahoo.com','$2b$12$fIJ4F0MzP1iUSruJJ3dACejo3nyYUZXps37P2hixki2EgD3bJFr5e','2023-11-20 13:40:56','normal',0,'unverified');
+INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (36,'admin','admin@gmail.com','$2b$12$rt5h18AnAGaSsZ6VEsfUb.Mv4ECXM9OxyZYvgjKuo7ObqBwaNT5TK','2023-11-20 05:19:30','admin',0,'verified');
+INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (37,'khim','khimrata@yahoo.com','$2b$12$fIJ4F0MzP1iUSruJJ3dACejo3nyYUZXps37P2hixki2EgD3bJFr5e','2023-11-20 13:40:56','normal',0,'verified');
 INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (38,'mamamo','mamamo@gmail.com','$2b$12$w6QbSW7HZ1ssjNTFRKncwusVzS28ZOnokaor27uuGQS9KFx.8HeLm','2023-11-20 05:40:37','normal',0,'unverified');
 INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (39,'kokey','kokey@gmail.com','$2b$12$tYPbOuUJlUM99owbkc.uROdEXOkxcxdM9I73d9Z7GNrxeocvYM./O','2023-11-21 12:22:14','normal',1,'unverified');
 INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (40,'iyak','iyak@gmail.com','$2b$12$fi6JPoQe54Q6tl5hduvzXODLxj9SHzm79T4uwx3w9dk4d2Hv4CaKG','2023-11-22 19:23:06','normal',1,'unverified');
