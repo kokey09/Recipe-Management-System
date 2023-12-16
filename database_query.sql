1ee91402a13034c1259094538cd35c6078c6f0bb
KHIM <146093792+kokey09@users.noreply.github.com>
2023-12-16 11:55:11 +0800

    .

--- database_query.sql
+++ database_query.sql
@@ -17,8 +17,8 @@ CREATE TABLE account (
     password VARCHAR(60),
     date_created TIMESTAMP,
     type VARCHAR(50),
-    is_deleted TINYINT(1)
-	status ENUM('unverified', 'verified') DEFAULT 'unverified',
+    is_deleted TINYINT(1),
+	status ENUM('unverified', 'verified') DEFAULT 'unverified'
 );
 
 CREATE TABLE recipes (
