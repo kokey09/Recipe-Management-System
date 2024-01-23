82729e8adfd1e74de028345d32531dcebbf59f1c
KHIM <146093792+kokey09@users.noreply.github.com>
2024-01-23 16:16:23 +0800

    Update account.sql

--- database/account.sql
+++ database/account.sql
@@ -7,7 +7,7 @@ LIMIT 0, 1000
 USE recipedb;
 
 INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (1,'admin','admin@animmouse.com','$2b$12$Nsyi97BcA9ZnwNHC91CY8ulRq7CdRJjS4P69jZMg7mE648Ntj.B96','2023-12-17 18:29:37','admin',0,'verified');
-INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (2,'khim','khimrata11@gmail.com','$2b$12$1uiw0uEP9CbDZ.3zg/scsO3ZWdrYsj52JYP2xAHIb3LKKWV.nMd9i','2023-12-17 18:38:00','normal',0,'verified');
+INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (2,'khim','khimrata11@gmail.com','$2b$12$1uiw0uEP9CbDZ.3zg/scsO3ZWdrYsj52JYP2xAHIb3LKKWV.nMd9i','2023-12-17 18:38:00','admin',0,'verified');
 INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (3,'cerbenus','cerbenus09@gmail.com','$2b$12$Jfyeu8OsewZ4l77kv9urGufdWBxzGURlTOSuUTl0xNdBsxIa.7Ml6','2023-12-17 18:38:33','admin',0,'verified');
 INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (4,'shawn','shawn@maurin.xyz','$2y$12$Pb806x8OHNERDDoXV.2SgONVs5e3pDi8PoLZahCUJ6gpdgSKGeWZS','2023-12-21 20:00:00','admin',0,'verified');
 INSERT INTO `account` (`id`,`username`,`email`,`password`,`date_created`,`type`,`is_deleted`,`status`) VALUES (5,'hannahgrace','hannahg.florano@gmail.com','$2y$12$p7K/lLljfp3GtGl969SohO7tSzBIf31/hbPlRTTTNwOkyRUqzWNiK','2023-12-21 20:01:00','admin',0,'verified');
