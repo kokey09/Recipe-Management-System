d9d58ba84f07832cbe73c7186ca99c92678dcc53
khim rata <khimrata11@gmail.com>
2024-02-17 21:11:27 +0800

    env

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from controllers import create_app
 from models.account import create_default_admin
 from waitress import serve
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = create_app()
+app.secret_key = os.getenv('SECRET_KEY')  # use secret key from environment variable
 create_default_admin(app)
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=5000)
     #below is for development
     # app.run(host="0.0.0.0", port=5000,debug=True)
-
--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 
 
+
+
 import os
 import cloudinary
+
 
 db = SQLAlchemy()
 mail = Mail()
@@ -14,7 +17,7 @@ cloudinary.config(
   api_key = "855798891421539", 
   api_secret = "j0nWfrODok-6Qf1KNASMcpePdyQ" 
 )
-
+ 
 
 print(os.getenv('CLOUDINARY_API_KEY'))
 
@@ -30,9 +33,9 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'khimrata11@gmail.com'  # Replace with your actual email
-    app.config['MAIL_PASSWORD'] = ''  # Replace with your App Password
-    app.secret_key = 'your_secret_key'
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  
+    app.secret_key = os.getenv('SECRET_KEY')
     
   
     db.init_app(app)
